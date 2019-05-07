eed0df507dfd72e77326bb6a0a39170fb511d840
tusharsaxena250 <44317411+tusharsaxena250@users.noreply.github.com>
2019-05-08 00:13:01 +0530

    Update loadDic.py

--- loadDic.py
+++ loadDic.py
@@ -1,40 +1,10 @@
 #import IR_GUI as gui
 import os
 
-DIR1 = "A:/IR1/New Folder"
-DIR2 = "//UTKARSH-1998/IR2"
+DIR1 = "/A..."
+DIR2 = "/B..."
 
 print(os.scandir(DIR1))
-
-#for roots, dirs, files in os.walk(DIRECTORY):
-#    for file in files:
-#        print("File = %s" %file)
-#    for dir in dirs :
-#        if (dir!='Users'): 
-#            print("File = %s" %dir)
-
-#roots = next(os.walk(DIR1))[0]
-#print("Roots= %s" %roots)
-
-#dirs = next(os.walk(DIR1))[1]
-#for d in dirs:
-#    dirfiles = next(os.walk(DIR1+"/"+d))[2]
-#    print("DIRFiles= %s" %dirfiles)
-#print("Directories = %s" %dirs)
-
-#print(gui.BButtons.res)
-
-#def parseDir(directory):
-#    dirs = next(os.walk(directory))[1]
-#    for d in dirs:
-#        dirfiles = next(os.walk(directory+"/"+d))[2]
-#        SearchConvertFiles(dirfiles, directory)
-#        
-#        nextDir = next(os.walk(dirs))[1]
-#        if nextDir.size:
-#            parseDir(nextDir)
-#        else:
-#            pass
 
 def SearchConvertFiles():
     global i
@@ -42,32 +12,18 @@ def SearchConvertFiles():
     files = next(os.walk(DIR1))[2]
     for file in files:
         i = i+1
-        f = open(file, "r", encoding='utf8')
-        if f:
-            contents = f.read()
-            
-            o = open("file"+str(i)+".txt", "w+", encoding='utf8')
-            o.write(contents)
-            o.close()
-            
-            f.close()
-            os.remove(file)
+        try:
+            f = open(file, "r", encoding='utf8')
+            if f:
+                contents = f.read()
 
-i = 2273
+                o = open("file"+str(i)+".txt", "w+", encoding='utf8')
+                o.write(contents)
+                o.close()
+
+                f.close()
+                os.remove(file)
+        except Exception: pass 
+i = 0
             
 SearchConvertFiles()
-
-#import urllib.request
-#import urllib.parse
-#import re
-#
-#url = 'http://localhost:8000'
-#values = {'s':'basics',
-#          'submit':'search'}
-#data = urllib.parse.urlencode(values)
-#data = data.encode('utf-8')
-#req = urllib.request.Request(url, data)
-#resp = urllib.request.urlopen(req)
-#respData = resp.read()
-#
-#print(respData)
